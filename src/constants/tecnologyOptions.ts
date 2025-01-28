231d105e6492dbbf1524c6e0c8d1058bad92c46a
Luiz Vinicius <viniciusgba2008@gmail.com>
2025-01-28 20:15:49 -0300

    feat: adicionando tecnologias

--- src/constants/tecnologyOptions.ts
+++ src/constants/tecnologyOptions.ts
@@ -1,48 +1,51 @@
 interface TecnologyOption {
   imageUrl: string
   title: string
-  className: string
 }
 
 export const TecnologyOption: TecnologyOption[] = [
   {
     imageUrl: '/react.svg',
     title: 'React',
-    className: 'bg-blue-500',
   },
   {
     imageUrl: '/typescript.svg',
     title: 'Typescript',
-    className: 'bg-blue-600',
   },
   {
     imageUrl: '/javascript.svg',
     title: 'Javascript',
-    className: 'bg-yellow-500',
   },
   {
     imageUrl: '/html.svg',
     title: 'HTML',
-    className: 'bg-orange-600',
   },
   {
     imageUrl: '/css.svg',
     title: 'CSS',
-    className: 'bg-blue-500',
   },
   {
     imageUrl: '/tailwind.svg',
     title: 'Tailwind CSS',
-    className: 'bg-teal-400',
   },
   {
     imageUrl: '/shadcn.svg',
     title: 'Shadcn',
-    className: 'bg-gray-800',
   },
   {
     imageUrl: '/vite.svg',
     title: 'Vite',
-    className: 'bg-purple-500',
+  },
+  {
+    imageUrl: '/nodejs.svg',
+    title: 'Node Js',
+  },
+  {
+    imageUrl: '/prisma.svg',
+    title: 'Prisma',
+  },
+  {
+    imageUrl: '/express.svg',
+    title: 'Express',
   },
 ]
